34b27a0fb3f9d030b9452ff6b1df4e5ebe3fef06
xela-trawets <xela.trawets@gmail.com>
2019-09-30 18:17:10 -0700

    ...

--- main.go
+++ main.go
@@ -8,14 +8,91 @@ import (
 	"syscall"
 	"unsafe"
 
-	"../GoArmTest/semaphore"
 	"golang.org/x/exp/mmap"
 )
 
+// #include <fcntl.h>           /* For O_* constants */
+// #include <sys/stat.h>        /* For mode constants */
+// #include <semaphore.h>
+// #include <string.h>
+// #include <stdlib.h>
+// #include <errno.h>
+// #include <stdio.h>
 /*
-#include <string.h>
+int _errno() {
+    return errno;
+}
+typedef struct {
+    sem_t*      val;
+    int         err;
+} sem_tt;
+sem_tt* _sem_open(char* name, int flags) {
+    sem_tt* r = (sem_tt*)malloc(sizeof(sem_tt));
+    sem_t* sem = sem_open((const char*)name, flags, 0644, 0);
+    if (sem == SEM_FAILED) r->err = errno;
+    else r->val = sem;
+    return r;
+}
+int _sem_close(void* sem) {
+    return sem_close(((sem_tt*)sem)->val) == 0 ? 0 : errno;
+}
+int _sem_wait(void* sem) {
+    return sem_wait(((sem_tt*)sem)->val) == 0 ? 0 : errno;
+}
+int _sem_trywait(void* sem) {
+    return sem_trywait(((sem_tt*)sem)->val) == 0 ? 0 : errno;
+}
+int _sem_post(void* sem) {
+    return sem_post(((sem_tt*)sem)->val) == 0 ? 0 : errno;
+}
+int _sem_unlink(char* name) {
+    return sem_unlink((const char*) name) == 0 ? 0 : errno;
+}
+int* pInt() {
+    int* val = (int*)malloc(sizeof(int));
+    return val;
+}
+const char* cpchar(char* val) {
+    return (const char*)val;
+}
+const struct timespec* new_timespec(time_t sec, long nsec) {
+    struct timespec* val = (struct timespec*)malloc(sizeof(struct timespec));
+    val->tv_sec = sec;
+    val->tv_nsec = nsec;
+    return (const struct timespec*)val;
+}
 */
 import "C"
+
+type semaphore struct {
+	name  string
+	cName *C.char
+	sema  unsafe.Pointer
+}
+
+func open(name string, excl bool) (Semaphore, error) {
+	name = fmt.Sprintf("/%s", name)
+	cName := C.CString(name)
+
+	flags := C.O_CREAT
+	if excl {
+		flags = flags | C.O_EXCL
+	}
+
+	sema := C._sem_open(cName, C.int(flags))
+	if sema.err != 0 {
+		return nil, goof.WithFields(goof.Fields{
+			"name":  name,
+			"error": sema.err,
+		}, "error opening semaphore")
+	}
+
+	return &semaphore{
+		name:  name,
+		cName: cName,
+		sema:  unsafe.Pointer(sema),
+	}, nil
+}
 
 func memcpy(dest, src []byte) int {
 	n := len(src)
